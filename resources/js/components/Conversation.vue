e6ace4df5cbcf977442e3ec084f842eeeee1f8cd
tiagogarcia178 <tiagobgarcia@icloud.com>
2021-12-02 14:25:08 +0000

    First commit

---
+++ resources/js/components/Conversation.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="conversation">
+    <h1>{{ contact ? contact.name : "Selecione um contacto" }}</h1>
+    <MessagesFeed :contact="contact" :messages="messages" />
+    <MessageComposer @send="sendMessage" />
+  </div>
+</template>
+
+<script>
+import MessagesFeed from "./MessagesFeed";
+import MessageComposer from "./MessageComposer";
+
+export default {
+  props: {
+    contact: {
+      type: Object,
+      default: null,
+    },
+    messages: {
+      type: Array,
+      default: [],
+    },
+  },
+  methods: {
+    sendMessage(text) {
+      console.log(this.contact.id);
+      console.log(text);
+
+      if(!this.contact) {
+        return;
+      }
+      axios.post("/conversation/send", {
+          contact_id: this.contact.id,
+          text: text  
+        }).then((response) => {
+          this.$emit('new', response.data);
+        })
+
+    }
+  },
+  components: { MessagesFeed, MessageComposer },
+};
+</script>
+
+<style lang="scss" scoped>
+.conversation {
+  flex: 5;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 20px;
+    padding: 10px;
+    margin: 0;
+    border-bottom: 1px dashed lightgray;
+  }
+}
+</style>
